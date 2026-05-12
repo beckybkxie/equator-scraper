7fe1ad019e5faef746f8ed666072bb23453bac82
equator <info@equatorca.com>
2026-05-12 12:49:19 -0700

    render deploy

--- containerdata/app.py
+++ containerdata/app.py
@@ -16,7 +16,14 @@ from selenium.common.exceptions import (
     StaleElementReferenceException
 )
 
-from webdriver_manager.chrome import ChromeDriverManager
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options=chrome_options)
+
 
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
